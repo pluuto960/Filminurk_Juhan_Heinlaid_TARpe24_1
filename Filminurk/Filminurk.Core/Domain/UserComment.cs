c3667357aa980be65106f2d62aa7d070c7aea8ab
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2025-11-15 17:21:24 +0200

    midagi on katki

---
+++ Filminurk/Filminurk.Core/Domain/UserComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class UserComment
+    {
+        [Key]
+        public Guid CommentID { get; set; }
+        public string? CommentUserID { get; set; }
+        public string CommentBody { get; set; }
+        public int CommentedScore { get; set; }
+        public int IsHelpful { get; set; } // Like
+        public int IsHarmful { get; set; } // Dislike
+
+        // Andmebaasi jaoks vajalikud andmed
+        public DateTime CommentCreatedAt { get; set; }
+        public DateTime CommentModifiedAt { get; set; }
+        public DateTime? CommentDeletedAt { get; set; }
+    }
+}
--- Filminurk/Filminurk.Core/ServiceInterface/IUserCommentsServices.cs
+++ Filminurk/Filminurk.Core/ServiceInterface/IUserCommentsServices.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+using Filminurk.Core.Dto;
 
 namespace Filminurk.Core.ServiceInterface
 {
     public interface IUserCommentsServices
     {
-        Task<UserComment> NewComment(UserCommentDTO newcommentDTO)
+        public Task<UserComment> NewComment(UserCommentDTO newcommentDTO);
+        public Task<UserComment> DetailsAsync(Guid id);
+        public Task<UserComment> Delete(Guid id);
     }
 }
